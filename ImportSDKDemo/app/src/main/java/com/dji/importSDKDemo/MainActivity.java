a8e7737570d0da4c9800616e1faa8390b2fbd750
OliverOu <oliver.ou@dji.com>
2016-09-09 11:48:41 +0800

    Update Sample Code to Android SDK 3.3.

--- ImportSDKDemo/app/src/main/java/com/dji/importSDKDemo/MainActivity.java
+++ ImportSDKDemo/app/src/main/java/com/dji/importSDKDemo/MainActivity.java
@@ -16,11 +16,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-import dji.sdk.SDKManager.DJISDKManager;
+import dji.sdk.sdkmanager.DJISDKManager;
 import dji.sdk.base.DJIBaseComponent;
 import dji.sdk.base.DJIBaseProduct;
-import dji.sdk.base.DJIError;
-import dji.sdk.base.DJISDKError;
+import dji.common.error.DJIError;
+import dji.common.error.DJISDKError;
 
 public class MainActivity extends AppCompatActivity {
 
